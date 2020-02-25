648e8e8b4139da62f869b3bcc7eaaf32ef8e0114
ThomasGalesloot <57332344+ThomasGalesloot@users.noreply.github.com>
2020-02-25 13:03:29 -0700

    initial commit

---
+++ Database.py
@@ -0,0 +1,11 @@
+import pyodbc
+
+
+class Database:
+
+    def __init__(self):
+        self.conn = pyodbc.connect('Driver={SQL Server};'
+                                   'Server=LAPTOP-GCB1I1ES;'
+                                   'Database=Users;'
+                                   'Trusted_Connection=yes;')
+        self.cursor = self.conn.cursor()
---
+++ Login.py
@@ -0,0 +1,29 @@
+from Database import Database
+
+
+# this is a simple test of the db feature
+class Login:
+    username = ""
+    password = ""
+    db = Database()
+    confirm_found = "false"
+
+    def __init__(self, na, pa):
+        self.username = na
+        self.password = pa
+
+    def compare(self):
+        rowcount = self.db.cursor.execute('select * from Users.dbo.login_info WHERE username =? AND password = ?',
+                                          (self.username, self.password))
+
+        rows = rowcount.fetchone()
+        print(rows)
+        if rows is None:
+            self.confirm_found = "false"
+        else:
+            self.confirm_found = "true"
+
+
+test = Login("SimpleNoodleMan", "1234Password")
+test.compare()
+print(test.confirm_found)
