781cf70a7733207a3850c05a1c4ae8c26641cf12
bert098 <robert.dumitru098@gmail.com>
2020-02-27 13:03:24 -0700

    Login

--- Login.py
+++ Login.py
@@ -26,27 +26,4 @@ class Login:
             self.confirm_found = "true"
 
 
-app = Flask(__name__)
 
-
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
-
-
-
-@app.route('/', methods=['POST'])
-def getvalue():
-    Username = request.form['uname']
-    Password = request.form['psw']
-    Test = Login(Username, Password)
-    Test.compare()
-    print(Test.confirm_found)
-    if Test.confirm_found == "true":
-        return render_template("Welcom.html")
-    else:
-        return render_template("NoGood.html")
-
-
-if __name__ == "__main__":
-    app.run()
--- profile.py
+++ profile.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-
+from Login import Login
+from Database import Database
 app = Flask(__name__)
 
 
@@ -8,5 +9,19 @@ def profile():
     return render_template("profile.html")
 
 
+@app.route('/', methods=['POST'])
+def getvalue():
+    Username = request.form['uname']
+    Password = request.form['psw']
+    Test = Login(Username, Password)
+    Test.compare()
+    print(Test.confirm_found)
+    if Test.confirm_found == "true":
+        return render_template("Welcom.html")
+    else:
+        return render_template("NoGood.html")
+
+
 if __name__ == "__main__":
     app.run()
+
