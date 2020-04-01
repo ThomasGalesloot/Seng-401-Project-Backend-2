63479fdcd93e1d08bf53e38eb739eef118160b83
Ryan Oakes <ryanoakes686@gmail.com>
2020-03-31 18:20:58 -0600

    Added searchComment, but needs a toSearch input first, for testing this is hard coded right now. Need to still implement turning results into JSON

--- Comment.py
+++ Comment.py
@@ -9,6 +9,7 @@ class Comment:
     author = ""
     votes = ""
     parentPostID = ""
+    retrievedComments = []
 
     def __init__(self, tit, cont, auth, votes, parentPostID):
         self.title = tit
@@ -23,7 +24,7 @@ class Comment:
 
         db = CommentsDatabase()
         #  cd = CommentData("title", "commentText", "author", "votes", "parentPostID")
-        retrievedComments = []
+        # retrievedComments = []
         self.insertComment()
 
     # inserts a brand new comment into the db
@@ -31,14 +32,27 @@ class Comment:
 
         # self.cd = CommentData("title", "commentText", "testing", "votes", "parentPostID")
         self.db.cursor.execute("INSERT INTO [dbo].[Comments] ("
-                                   "[postID]"
-                                   ",[userID]"
-                                   ",[votes]"
-                                   ",[cmtText])"
-                                   "VALUES"
-                                   "( " + str(self.parentPostID) + # TODO make this get the post ID from the event table, also why does this need to be string, also removed ''
-                                    ", '" + self.author +
-                                    "', " + str(0) +
-                                    ", '" + self.commentText + "')")
+                               "[postID]"
+                               ",[userID]"
+                               ",[votes]"
+                               ",[cmtText])"
+                               "VALUES"
+                               "( " + str(self.parentPostID) +  # TODO make this get the post ID from the event table
+                               ", '" + self.author +
+                               "', " + str(0) +
+                               ", '" + self.commentText + "')")
 
         self.db.conn.commit()
+        self.searchComment(0)
+
+    def searchComment(self, toSearch):
+        self.db.cursor.execute(
+            'select * from [dbo].[Comments] where postID =' + str(0))  #TODO replace 0 with toSearch when done testing
+        for row in self.db.cursor.fetchall():
+            self.retrievedComments.append(
+                CommentData("title", row[0], row[1], row[2], row[3], row[4]))  # TODO add title to db and implement
+        for i in self.retrievedComments:
+            print(i.title + "," + i.commentText + "," +
+                  i.author + "," + str(i.votes) + "," +
+                  str(i.parentPostID) + "\n")  # May need to put votes and ids in str()
+
--- CommentData.py
+++ CommentData.py
@@ -1,12 +1,14 @@
 class CommentData:
     title = ""
+    commentID = ""
     commentText = ""
     author = ""
     votes = ""
     parentPostID = ""
 
-    def __init__(self, tit, text, auth, votes,  ppID):
+    def __init__(self, tit, cID, ppID, auth, votes, text):
         self.title = tit
+        self.commentID = cID
         self.commentText = text
         self.votes = votes
         # self.datePosted = dp
