fed0417ec1648f462983eb51afc336d8117e410c
Terry <terryhung888@gmail.com>
2020-04-02 13:15:00 -0600

    Adding in the true/false condition on password

--- Search.py
+++ Search.py
@@ -8,11 +8,11 @@ class Search:
 
     # Will search posts based on title alone
     def searchPostsByTitle(self, searchQuery):
-        self.db.cursor.execute('select * from Project.dbo.posts_info where Title like "?ham?"')
+        self.db.cursor.execute('select * from Project.dbo.posts_info where Title like \'%' + searchQuery + "%\'")
         allPosts = self.db.cursor.fetchall()
         for row in allPosts:
             if row[2] is not None and row[3] is not None:
-                self.retrievedPost.append(PostData(row[1], row[2], row[3], row[4], row[5], row[6], row[7],row[0]))
+                self.retrievedPost.append(PostData(row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[0]))
 
     # Will search posts by type alone
     def searchPostsByType(self, searchQuery):
